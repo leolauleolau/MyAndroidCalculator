bb5ecb7492a3c02a33d45741327d8643cc499f4b
leolau <leolauleolau@gmail.com>
2021-08-16 09:20:02 +0800

    add button listener

--- app/src/main/java/com/example/mycalculator/MainActivity.java
+++ app/src/main/java/com/example/mycalculator/MainActivity.java
@@ -3,12 +3,64 @@ package com.example.mycalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText result;
+    private EditText newNumber;
+    private TextView displayOperation;
 
+    private Double operand1 = null;
+    private Double operand2 = null;
+    private String pendingOperation = "=";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        result = findViewById(R.id.result);
+        newNumber = findViewById(R.id.inputNumber);
+        displayOperation = findViewById(R.id.operator);
+
+        Button button0 = findViewById(R.id.button0);
+        Button button1 = findViewById(R.id.button1);
+        Button button2 = findViewById(R.id.button2);
+        Button button3 = findViewById(R.id.button3);
+        Button button4 = findViewById(R.id.button4);
+        Button button5 = findViewById(R.id.button5);
+        Button button6 = findViewById(R.id.button6);
+        Button button7 = findViewById(R.id.button7);
+        Button button8 = findViewById(R.id.button8);
+        Button button9 = findViewById(R.id.button9);
+
+        Button buttonDot = findViewById(R.id.buttonDot);
+        Button buttonEquals = findViewById(R.id.buttonEqual);
+        Button buttonDivide = findViewById(R.id.buttonDiv);
+        Button buttonMultiply = findViewById(R.id.buttonMultiply);
+        Button buttonMinus = findViewById(R.id.buttonMinus);
+        Button buttonPlus = findViewById(R.id.buttonplus);
+        View.OnClickListener listner = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Button b =(Button) v;
+                newNumber.append(b.getText().toString());
+            }
+        };
+        button0.setOnClickListener(listner);
+        button1.setOnClickListener(listner);
+        button2.setOnClickListener(listner);
+        button3.setOnClickListener(listner);
+        button4.setOnClickListener(listner);
+        button5.setOnClickListener(listner);
+        button6.setOnClickListener(listner);
+        button7.setOnClickListener(listner);
+        button8.setOnClickListener(listner);
+        button9.setOnClickListener(listner);
+        buttonDot.setOnClickListener(listner);
+
+
     }
 }
